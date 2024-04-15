c1fa421e848f2849cb087599e0b187f6730a5e27
IcelandVlog <167001974+IcelandVlog@users.noreply.github.com>
2024-04-16 00:27:50 +0600

    Add files via upload

---
+++ slider-data.js
@@ -0,0 +1,32 @@
+let movies = [
+    {
+        name: 'T20 World Cup 2024',
+        des: 'Join us for ICC Men-s T20 World Cup as this June West Indie-s and the USA..',
+        image: 'img-1.png'
+    },
+    {
+        name: 'Rudra: The Edge of Darkness',
+        des: 'Rudra: The Edge of Darkness is an Indian, created for Disney+ Hotstar. It is a remake of British series Luther',
+        image: 'slider 1.PNG'
+    },
+    {
+        name: 'The Book of Boba Fett',
+        des: 'The Book of Boba Fett is an American space Western television rvice Disney+',
+        image: 'slider 3.PNG'
+    },
+    {
+        name: 'raya and the last dragon',
+        des: 'Raya and the Last Dragon is a 2021 American animated fantasy Disney Studios Motion Pictures',
+        image: 'slider 4.PNG'
+    },
+    {
+        name: 'Luca(2021)',
+        des: 'Luca is a 2021 American animated  fantasy film Disney Pictures and Pixar Animation',
+        image: 'slider 5.PNG'
+    },
+    {
+        name: 'Rudra',
+        des: 'Rudra: The Edge of Darkness is an Indian, created for Disney+ Hotstar. It is a remake of British series Luther',
+        image: 'slider 1.PNG'
+    },
+]
